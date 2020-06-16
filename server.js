6f8e8112ee7b1ab4149207950a2927d71f202a81
David <david.l.larsson.90@gmail.com>
2020-06-16 18:25:20 +0200

    typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use((req, res, next) => {
-    console.log(req.method + " " + req.path + " " + req.id);
+    console.log(req.method + " " + req.path + " " + req.ip);
     next();
   });
 app.listen(PORT, () => {
